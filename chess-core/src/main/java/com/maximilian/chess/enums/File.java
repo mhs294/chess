8286692c02d983e4f4a1bfce204bac96f60db22b
Maximilian Schroeder <mhs294@mst.edu>
2019-06-30 16:48:04 -0500

    Fixed incorrect Square/File definitions

--- chess-core/src/main/java/com/maximilian/chess/enums/File.java
+++ chess-core/src/main/java/com/maximilian/chess/enums/File.java
@@ -19,6 +19,62 @@ import static com.maximilian.chess.enums.Square.A5;
 import static com.maximilian.chess.enums.Square.A6;
 import static com.maximilian.chess.enums.Square.A7;
 import static com.maximilian.chess.enums.Square.A8;
+import static com.maximilian.chess.enums.Square.B1;
+import static com.maximilian.chess.enums.Square.B2;
+import static com.maximilian.chess.enums.Square.B3;
+import static com.maximilian.chess.enums.Square.B4;
+import static com.maximilian.chess.enums.Square.B5;
+import static com.maximilian.chess.enums.Square.B6;
+import static com.maximilian.chess.enums.Square.B7;
+import static com.maximilian.chess.enums.Square.B8;
+import static com.maximilian.chess.enums.Square.C1;
+import static com.maximilian.chess.enums.Square.C2;
+import static com.maximilian.chess.enums.Square.C3;
+import static com.maximilian.chess.enums.Square.C4;
+import static com.maximilian.chess.enums.Square.C5;
+import static com.maximilian.chess.enums.Square.C6;
+import static com.maximilian.chess.enums.Square.C7;
+import static com.maximilian.chess.enums.Square.C8;
+import static com.maximilian.chess.enums.Square.D1;
+import static com.maximilian.chess.enums.Square.D2;
+import static com.maximilian.chess.enums.Square.D3;
+import static com.maximilian.chess.enums.Square.D4;
+import static com.maximilian.chess.enums.Square.D5;
+import static com.maximilian.chess.enums.Square.D6;
+import static com.maximilian.chess.enums.Square.D7;
+import static com.maximilian.chess.enums.Square.D8;
+import static com.maximilian.chess.enums.Square.E1;
+import static com.maximilian.chess.enums.Square.E2;
+import static com.maximilian.chess.enums.Square.E3;
+import static com.maximilian.chess.enums.Square.E4;
+import static com.maximilian.chess.enums.Square.E5;
+import static com.maximilian.chess.enums.Square.E6;
+import static com.maximilian.chess.enums.Square.E7;
+import static com.maximilian.chess.enums.Square.E8;
+import static com.maximilian.chess.enums.Square.F1;
+import static com.maximilian.chess.enums.Square.F2;
+import static com.maximilian.chess.enums.Square.F3;
+import static com.maximilian.chess.enums.Square.F4;
+import static com.maximilian.chess.enums.Square.F5;
+import static com.maximilian.chess.enums.Square.F6;
+import static com.maximilian.chess.enums.Square.F7;
+import static com.maximilian.chess.enums.Square.F8;
+import static com.maximilian.chess.enums.Square.G1;
+import static com.maximilian.chess.enums.Square.G2;
+import static com.maximilian.chess.enums.Square.G3;
+import static com.maximilian.chess.enums.Square.G4;
+import static com.maximilian.chess.enums.Square.G5;
+import static com.maximilian.chess.enums.Square.G6;
+import static com.maximilian.chess.enums.Square.G7;
+import static com.maximilian.chess.enums.Square.G8;
+import static com.maximilian.chess.enums.Square.H1;
+import static com.maximilian.chess.enums.Square.H2;
+import static com.maximilian.chess.enums.Square.H3;
+import static com.maximilian.chess.enums.Square.H4;
+import static com.maximilian.chess.enums.Square.H5;
+import static com.maximilian.chess.enums.Square.H6;
+import static com.maximilian.chess.enums.Square.H7;
+import static com.maximilian.chess.enums.Square.H8;
 import static com.maximilian.chess.objects.Board.EMPTY_BITMASK;
 
 /**
@@ -30,13 +86,13 @@ import static com.maximilian.chess.objects.Board.EMPTY_BITMASK;
 @Accessors(fluent = true)
 public enum File {
     A(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    B(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    C(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    D(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    E(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    F(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    G(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8)),
-    H(EnumSet.of(A1, A2, A3, A4, A5, A6, A7, A8));
+    B(EnumSet.of(B1, B2, B3, B4, B5, B6, B7, B8)),
+    C(EnumSet.of(C1, C2, C3, C4, C5, C6, C7, C8)),
+    D(EnumSet.of(D1, D2, D3, D4, D5, D6, D7, D8)),
+    E(EnumSet.of(E1, E2, E3, E4, E5, E6, E7, E8)),
+    F(EnumSet.of(F1, F2, F3, F4, F5, F6, F7, F8)),
+    G(EnumSet.of(G1, G2, G3, G4, G5, G6, G7, G8)),
+    H(EnumSet.of(H1, H2, H3, H4, H5, H6, H7, H8));
 
     private static final Map<Square, File> SQUARE_TO_FILE_MAP;
 
